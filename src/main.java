34a6fff77fa36670091acc553f2898c72b7ac5fc
jakubstawiarz <kuba.stawiarz123@wp.pl>
2021-11-19 20:52:26 +0100

    modyfikacja5

--- src/main.java
+++ src/main.java
@@ -1,12 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        Auto[] a = new Auto[20];
+       
 
-        a[0] = new Auto("dsadasd",21,"audi");
-        a[0] = new Auto("dsadasd",21,"audi");
-        a[0] = new Auto("dsadasd",21,"audi");
-        a[0] = new Auto("dsadasd",21,"audi");
-
-        a[0].wyswietl();
     }
 }
