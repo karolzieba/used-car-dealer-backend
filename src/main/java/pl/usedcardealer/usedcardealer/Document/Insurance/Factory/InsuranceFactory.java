acc14ed9a576fe7d312cb1a497fbc68ed75b2f23
Karol Zięba <kz516749@gmail.com>
2022-01-31 01:19:27 +0100

    zmiany

--- src/main/java/pl/usedcardealer/usedcardealer/Document/Insurance/Factory/InsuranceFactory.java
+++
@@ -1,9 +0,0 @@
-/*package pl.usedcardealer.usedcardealer.Document.Insurance.Factory;
-
-import pl.usedcardealer.usedcardealer.Car.Car;
-
-import java.util.Date;
-
-public interface InsuranceFactory <T> {
-    T createInsurance(Date date, Car car, Date startDate, Date endDate, double price);
-}*/
